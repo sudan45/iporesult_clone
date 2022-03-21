c54899454d3034f95d6780d30fca0aaded34434c
Haddi <sudan.bhandari@newitventure.com>
2022-03-21 15:14:39 +0545

    check api implemented

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CompanydataService } from '../service/companydata.service';
+import { FormGroup, FormControl } from '@angular/forms';
+
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.css'],
 })
 export class FormsComponent implements OnInit {
-  data_company: any;
+
+  companies: any = [];
   brand_image = './assets/brand.png';
-  constructor(private companydata: CompanydataService) {
-    companydata.company().subscribe((data) => {
-      this.data_company = data;
+  constructor(private companyData: CompanydataService) {
+    companyData.company().subscribe((response: any) => {
+      console.log(response['body'])
+      this.companies = response['body'];
     });
   }
-  checkresult(data:any){
-    let req_data={
-      "companyShareId":data.company,
-      "boid":data.boid
-    }
-    console.log(req_data)
-    console.log(data)
-    
-  }
+
+  iporesult = new FormGroup({
+    companyShareId: new FormControl(),
+    boid: new FormControl(''),
+  });
+
 
   ngOnInit(): void {
-    
+
+  }
+
+
+  ipo_result() {
+
+    this.companyData.check_result(this.iporesult.value).subscribe((res: any) => {
+      console.log(res)
+      console.log('valid')
+    })
+
+    console.log(this.iporesult.value)
+
   }
 }
--- src/app/service/companydata.service.ts
+++ src/app/service/companydata.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Observable, throwError } from 'rxjs';
+
 
 
 @Injectable({
@@ -12,7 +14,8 @@ check_url="https://iporesult.cdsc.com.np/result/result/check"
   company(){
     return this.http.get(this.url)
   }
-  // check_result(data:any){
-  //   return this.http.post(this.check_url,data)
-  // }
+  check_result(data:any){
+    const headers = { 'content-type': 'application/json' ,'Accept':"application/json"}  
+    return this.http.post(this.check_url,data,{'headers':headers})
+  }
 }
