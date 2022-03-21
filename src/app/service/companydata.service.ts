ef36cdc23bf427658b381e1079a719939e1ee94f
Sudan <sudan.bhandari@newitventure.com>
2022-03-21 21:02:04 +0545

    deployment in github

--- src/app/service/companydata.service.ts
+++ src/app/service/companydata.service.ts
@@ -15,7 +15,7 @@ check_url="https://iporesult.cdsc.com.np/result/result/check"
     return this.http.get(this.url)
   }
   check_result(data:any){
-    const headers = { 'content-type': 'application/json' ,'Accept':"application/json"}  
+    let headers = { 'content-type': 'application/json' ,'Accept':"application/json"}  
     return this.http.post(this.check_url,data,{'headers':headers})
   }
 }
